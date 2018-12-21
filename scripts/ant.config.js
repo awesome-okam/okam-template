cbcdb79cfea365a025967a54fa0a50fd600d0813
wuhy <sparklewhy@gmail.com>
2018-12-21 17:37:58 +0800

    fix ant app build config

--- scripts/ant.config.js
+++ scripts/ant.config.js
@@ -13,18 +13,15 @@ module.exports = merge({}, require('./base.config'), {
         depDir: 'src/common'
     },
 
+    // localPolyfill: ['async'],
+
     processors: {
         babel7: {
-            hook: {
-                before(file, options) {
-                    if (file.path.indexOf('/@babel/runtime/helpers/') !== -1) {
-                        options.plugins = [];
-                        options.presets = [];
-                    }
-                }
-            },
             options: {
-                presets: ['@babel/preset-env'],
+                presets: [['@babel/preset-env', {
+                    // exclude 这个转换，避免出现 babel helper typeof 自己引用自己问题
+                    exclude: ['transform-typeof-symbol']
+                }]],
                 plugins: [
                     ['@babel/plugin-transform-runtime', {
                         corejs: false,
